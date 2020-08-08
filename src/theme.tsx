d5a1fc22864d2fb0785aed2fd13a4d45825fc08b
kaitlynhova <kaitlynhova@gmail.com>
2020-08-08 09:46:24 -0700

    adding theme

---
+++ src/theme.tsx
@@ -0,0 +1,13 @@
+const palette = {
+  background: "#FFFFFF",
+  table: ["#FA7B06", "#FB5720", "#FC6E21", "#FDAE4E", "#FD8325"],
+  text: {
+    paragraph: "#FFFFFF",
+    navigation: "#000000",
+    table: "#000000",
+  },
+};
+
+export const theme = {
+  palette,
+};
